b8e47a1d047b51b75753d91a781aa22ce8edb70e
Stephen Arancio <sjarancio@gmail.com>
2021-06-28 17:25:50 -0400

    commit tree 957477bbbdf24f4d253e729fb9c8fbeb85929ac0

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet<= 400){
+    return "This one is on me!"
+  } if(feet<2500){
+    return 'I will gladly take your thirty bucks.'
+  }else{
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if( city === "NYC"){return 'Ok, sounds good.'
+} else{ return "No go."
+}
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+} else if (tip === "not as generous") {
+    return "Thank you.";
+} else {
+    return "Bye.";
 }
+}
+
+
